0afbc5c0a23a705c3c3f31b3330fa8dae03d9983
Nithin <nithin@Lokeswaraswamys-MacBook-Pro.local>
2026-04-09 19:35:21 +0530

    Tester1 conflict final change

--- src/test/java/com/sample/Sample.java
+++ src/test/java/com/sample/Sample.java
@@ -4,33 +4,29 @@ public class Sample {
 	
 	private void one() {
 		System.out.println("one");
-
 	}
 	
 	private void two() {
 		System.out.println("two");
-
 	}
 	
 	private void three() {
 		System.out.println("three");
-
 	}
 	
 	private void ConflictMethod() {
-	    System.out.println("ConflictMethod");
+	    System.out.println("Tester1 change");
 	}
 	
 	private void ConflictMethod2() {
-	    System.out.println("ConflictMethod2");
+	    System.out.println("Tester1 change 2");
 	}
 	
 	private void ConflictMethod3() {
-	    System.out.println("ConflictMethod3");
+	    System.out.println("Tester1 change 3");
 	}
 	
 	private void ConflictMethod4() {
-	    System.out.println("ConflictMethod4");
+	    System.out.println("Tester1 change 4");
 	}
-
 }
